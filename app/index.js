1b13654b88efe50cbda7eb02445956649fb365dd
John Knotts <jhnnyk@gmail.com>
2024-09-26 13:00:31 -0600

    full reset

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function App() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello, edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello World!, edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
