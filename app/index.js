dce5db249c0aec6ad485baf15b54948909773796
John Knotts <jhnnyk@gmail.com>
2024-09-26 13:27:45 -0600

    basic layout

--- app/index.js
+++ app/index.js
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello World!, edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>list</Text>
+        </View>
+      </SafeAreaView>
+      <StatusBar style="auto" />
+    </>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  search: {
+    backgroundColor: 'green',
+    padding: 16,
+  },
+
+  list: {
+    backgroundColor: 'blue',
+    padding: 16,
+    flex: 1,
+  },
+})
