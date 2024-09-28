b9d71531b43857e479ae8fb971f45b9cfc96354a
John Knotts <jhnnyk@gmail.com>
2024-09-28 12:38:59 -0600

    adding the address

--- app/features/restaurants/components/restaurant-card.component.js
+++ app/features/restaurants/components/restaurant-card.component.js
@@ -10,9 +10,19 @@ const RestaurantCardCover = styled(Card.Cover)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `
 
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
+const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
+`
+
+const Title = styled.Text`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`
+
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
   color: ${(props) => props.theme.colors.ui.primary};
 `
 
@@ -32,7 +42,10 @@ export const RestaurantCard = ({ restaurant = {} }) => {
   return (
     <StyledRestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <Address>{address}</Address>
+      </Info>
     </StyledRestaurantCard>
   )
 }
