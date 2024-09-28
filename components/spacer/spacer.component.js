9f97c05304ed2259b67fb21812375a8a23fc3c9c
John Knotts <jhnnyk@gmail.com>
2024-09-28 15:42:49 -0600

    optimizing the spacer component

--- components/spacer/spacer.component.js
+++ components/spacer/spacer.component.js
@@ -1,38 +1,31 @@
 import styled from 'styled-components/native'
 
-const TopSmall = styled.View`
-  margin-top: ${(props) => props.theme.space[1]};
-`
-const TopMedium = styled.View`
-  margin-top: ${(props) => props.theme.space[2]};
-`
-const TopLarge = styled.View`
-  margin-top: ${(props) => props.theme.space[3]};
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+}
+
+const positionVariant = {
+  top: 'margin-top',
+  left: 'margin-left',
+  right: 'margin-right',
+  bottom: 'margin-bottom',
+}
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size]
+  const property = positionVariant[position]
+  const value = theme.space[sizeIndex]
+
+  return `${property}:${value}`
+}
+
+export const Spacer = styled.View`
+  ${({ position, size, theme }) => getVariant(position, size, theme)}
 `
 
-const LeftSmall = styled.View`
-  margin-left: ${(props) => props.theme.space[1]};
-`
-const LeftMedium = styled.View`
-  margin-left: ${(props) => props.theme.space[2]};
-`
-const LeftLarge = styled.View`
-  margin-left: ${(props) => props.theme.space[3]};
-`
-
-export const Spacer = ({ variant }) => {
-  switch (variant) {
-    case 'top.medium':
-      return <TopMedium />
-    case 'top.large':
-      return <TopLarge />
-    case 'left.small':
-      return <LeftSmall />
-    case 'left.medium':
-      return <LeftMedium />
-    case 'left.large':
-      return <LeftLarge />
-    default:
-      return <TopSmall />
-  }
+Spacer.defaultProps = {
+  position: 'top',
+  size: 'small',
 }
